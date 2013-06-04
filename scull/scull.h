0a7370b6375da016346b328f85fc9e2ec243ab5a
zWaR <myemail@guess.it>
2013-06-04 18:05:52 +0000

    added scull_write

--- scull/scull.h
+++ scull/scull.h
@@ -47,7 +47,7 @@ struct scull_qset {
 
 loff_t  scull_llseek    (struct file*, loff_t, int);
 ssize_t scull_read      (struct file*, char __user*, size_t, loff_t*);
-ssize_t scull_write     (struct file*, const char __user *, size_t, loff_t*);
+ssize_t scull_write     (struct file*, const char __user*, size_t, loff_t*);
 int     scull_ioctl     (struct inode*, struct file*, unsigned int, unsigned long);
 int     scull_open      (struct inode*, struct file*);
 int     scull_release   (struct inode*, struct file*);
