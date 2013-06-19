0803c343b9c1e0173e138fc9ff694759c053592e
zWaR <myemail@guess.it>
2013-06-19 19:30:04 +0000

    setconsole and setlevel added

---
+++ misc-progs/setconsole.c
@@ -0,0 +1,29 @@
+
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv)
+{
+    char bytes[2] = {11,0};
+    
+    if (argc == 2)
+        bytes[1] = atoi(argv[1]);
+    else
+    {
+        fprintf(stderr, "%s: need a single arg\n", argv[0]);
+        exit(1);
+    }
+    
+    if (ioctl(STDIN_FILENO, TIOCLINUX, bytes) < 0)
+    {
+        fprintf(stderr, "%s: ioctl(stdion, TIOCLINUX): %s\n", argv[0], strerror(errno));
+        exit(1);
+    }
+    
+    exit(0);
+}
---
+++ misc-progs/setlevel.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define __LIBRARY__
+#include <linux/unistd.h>
+
+_syscall3(int, syslog, int, type, char *, bufp, int, len);
+
+int main(int argc, char **argv)
+{
+    int level;
+    
+    if (argc == 2)
+    {
+        level = atoi(argv[1]);
+    }
+    else
+    {
+        fprintf(stderr, "%s: need a single arg\n", argv[0]);
+        exit(1);
+    }
+    
+    if (syslog(8, NULL, level) < 0)
+    {
+        fprintf(stderr, "%s: syslog(setlevel): %s\n", argv[0], strerror(errno));
+        exit(1);
+    }
+    exit(0);
+}
+
